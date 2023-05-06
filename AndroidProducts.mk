3d18f938b6f6f9244b98e3f44efd3cd75b491c48
Santhanabalan <bala2102002@gmail.com>
2023-05-06 15:55:00 +0530

    Pixelify
    Initial PE Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_vili.mk
+    $(LOCAL_DIR)/aosp_vili.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_vili-user \
-    xtended_vili-userdebug \
-    xtended_vili-eng
+    aosp_vili-user \
+    aosp_vili-userdebug \
+    aosp_vili-eng
